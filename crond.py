c7d2a94c8d39d904263a450d75fd849e3679cc27
Leo <leochan2017@gmail.com>
2018-09-21 14:09:15 +0800

    163邮箱返回554

---
+++ crond.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+# !/usr/bin/env python
+
+import os
+import time
+import smtplib
+
+'''
+发送邮件函数
+已经指定发送邮箱和接收邮箱
+标题为故障时间
+'''
+
+
+def sendMail():
+    mail_host = 'smtp.163.com' # 设置服务器
+    mail_port = '25' # 服务器端口
+    mail_sender = 'xxxxxx@163.com' # 发送用户名
+    mail_pass = 'xxxxxx' # 发送密码 (163是授权码，不是密码)
+    mail_receivers = ['xxxxxx@qq.com'] # 接收邮箱
+    message = 'Your Process is Dead at ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+    try:
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(mail_host, mail_port)
+        state = smtpObj.login(mail_sender, mail_pass)
+        if state[0] == 235:
+            smtpObj.sendmail(mail_sender, mail_receivers, message)
+            print '邮件发送成功'
+        smtpObj.quit()
+    except smtplib.SMTPException, e:
+        print str(e)
+
+'''
+检查进程是否抽了
+发现距离现在时间超过10分钟，则判断为进程抽了
+'''
+
+
+def checkProcess():
+    # fileModifyTime = os.popen('stat -c %Y nohup.out').read().split('\n')[0]
+    fileModifyTime = '1537437334'
+
+    fileModifyTime = int(fileModifyTime)
+
+    nowTime = int(time.time())
+
+    # 相差的秒数
+    difference = nowTime - fileModifyTime
+
+    # 如果10分钟没有反应，就证明他挂了呀
+    if difference > 600:
+        print '挂了呀'
+        sendMail()
+
+
+if __name__ == '__main__':
+    checkProcess()
