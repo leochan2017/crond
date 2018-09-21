c869860acb27a1c6528f8857670459789f261db4
Leo <leochan2017@gmail.com>
2018-09-21 14:57:12 +0800

    完成自动发信部分

--- crond.py
+++ crond.py
@@ -4,7 +4,8 @@
 import os
 import time
 import smtplib
-
+from email.mime.text import MIMEText
+from email.Header import Header
 '''
 发送邮件函数
 已经指定发送邮箱和接收邮箱
@@ -15,17 +16,23 @@ import smtplib
 def sendMail():
     mail_host = 'smtp.163.com' # 设置服务器
     mail_port = '25' # 服务器端口
-    mail_sender = 'xxxxxx@163.com' # 发送用户名
+    mail_sender = 'xxxxx@163.com' # 发送用户名
     mail_pass = 'xxxxxx' # 发送密码 (163是授权码，不是密码)
-    mail_receivers = ['xxxxxx@qq.com'] # 接收邮箱
-    message = 'Your Process is Dead at ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    mail_receivers = 'leochan2017@gmail.com' # 接收邮箱
+    Subject = '卧槽，Python进程又阻塞了'
+    Content = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+    message = MIMEText(Content, 'plain', 'utf-8')
+    message['Subject'] = Header(Subject, 'utf-8')
+    message['From'] = mail_sender
+    message['To'] = mail_receivers
 
     try:
         smtpObj = smtplib.SMTP()
         smtpObj.connect(mail_host, mail_port)
         state = smtpObj.login(mail_sender, mail_pass)
         if state[0] == 235:
-            smtpObj.sendmail(mail_sender, mail_receivers, message)
+            smtpObj.sendmail(mail_sender, mail_receivers, message.as_string())
             print '邮件发送成功'
         smtpObj.quit()
     except smtplib.SMTPException, e:
